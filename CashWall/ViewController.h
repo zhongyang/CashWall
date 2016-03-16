cb8df77fdb6564a66caf72c8e77de73870df18fb
zhongyang <zhongyang@cmss.chinamobile.com>
2016-03-16 17:38:03 +0800

    Initial commit

---
+++ CashWall/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CashWall
+//
+//  Created by zhongyang on 16/3/16.
+//  Copyright © 2016年 zhongyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CashWall/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CashWall
+//
+//  Created by zhongyang on 16/3/16.
+//  Copyright © 2016年 zhongyang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
